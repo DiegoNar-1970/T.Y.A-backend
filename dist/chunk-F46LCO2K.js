9f8826ab43287d89f01d7c9bac7de1bff6572f71
dinaranjo <diegonaranjo303@gmail.com>
2025-05-19 17:44:22 -0500

    ...

---
+++ dist/chunk-F46LCO2K.js
@@ -0,0 +1,13 @@
+// node_modules/@aws-sdk/core/dist-es/submodules/client/setCredentialFeature.js
+function setCredentialFeature(credentials, feature, value) {
+  if (!credentials.$source) {
+    credentials.$source = {};
+  }
+  credentials.$source[feature] = value;
+  return credentials;
+}
+
+export {
+  setCredentialFeature
+};
+//# sourceMappingURL=chunk-F46LCO2K.js.map
