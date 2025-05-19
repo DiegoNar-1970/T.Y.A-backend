b02327b22f62359f0e4ca5cf2236aa51785bc35b
dinaranjo <diegonaranjo303@gmail.com>
2025-05-19 17:34:47 -0500

    no se que estoy haciendo la verdad

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,25 @@
+import path from 'path'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/app.ts'],
-  outDir: 'dist',
-  format: ['esm'],
-  target: 'es2022',
+  format: ['esm'], // o 'cjs' si usas CommonJS
+  target: 'node18', // o tu versión Node
   sourcemap: true,
-  splitting: false,
-  clean: true
+  outDir: 'dist',
+  clean: true,
+  dts: false, // true si usas .d.ts
+  esbuildOptions(options) {
+    options.alias = {
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@controllers': path.resolve(__dirname, 'src/controllers'),
+        '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+        '@middleware': path.resolve(__dirname, 'src/middleware'),
+        '@models': path.resolve(__dirname, 'src/models'),
+        '@routes': path.resolve(__dirname, 'src/routes'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+      }
+  }
 })
+    
